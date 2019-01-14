5cf3f2f0ea2ba933a02aa86b530fa33d4e1e25b1
wu <18007093573@163.com>
2019-01-14 18:39:22 +0800

    增加背景时钟

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,11 +2,18 @@
   <transition name="fade">
     <div class="home">
       <div class="clock">
-        <!--<canvas  v-circle width="310" height="310">-->
+        <canvas  v-circle width="620" height="620" style="width: 310px; height: 310px ">
 
-        <!--</canvas >-->
+        </canvas >
       </div>
-      <div v-progress="param"></div>
+      <div class="clock1">
+        <canvas  v-circle1 width="620" height="620" style="width: 310px; height: 310px ">
+
+        </canvas >
+      </div>
+      <hr>
+      <div class="line" v-progress="param"></div>
+      <hr>
       <div class="circle" v-progress="param1">
         <div class="circleTitle">
           {{param1.count+'%'}}
@@ -36,15 +43,15 @@
           circle:function (el) {
             setInterval(function () {
               const ctx=el.getContext("2d");
-              ctx.clearRect(0, 0, 300, 300);
+              ctx.clearRect(0, 0, 600, 600);
               //console.log(ctx);
               //修改坐标原点
               ctx.save();
-              ctx.translate(155, 155);
+              ctx.translate(310, 310);
               //画圆盘
               ctx.beginPath();
-              ctx.lineWidth = 2;
-              ctx.arc(0,0,150,0,2*Math.PI);
+              ctx.lineWidth = 4;
+              ctx.arc(0,0,300,0,2*Math.PI);
               ctx.stroke();
               //画表盘
               for(let i=1; i<=60; i++){
@@ -54,16 +61,17 @@
                 ctx.strokeStyle = i % 5 ? "blue" : "red";
                 if(i%5==0){
                   ctx.save();
-                  ctx.moveTo(0, -140);
-                  ctx.lineTo(0, -150);
-                  ctx.translate(0, -130);
+                  ctx.moveTo(0, -280);
+                  ctx.lineTo(0, -300);
+                  ctx.translate(0, -240);
                   ctx.rotate(-(Math.PI / 180)*i*6);
                   ctx.textAlign='center';
-                  ctx.fillText(i/5, 0, 0,10);
+                  ctx.font = "40px 雅黑";
+                  ctx.fillText(i/5, 0, 0);
                   ctx.restore();
                 }else {
-                  ctx.moveTo(0, -145);
-                  ctx.lineTo(0, -150);
+                  ctx.moveTo(0, -290);
+                  ctx.lineTo(0, -300);
                 }
                 ctx.closePath();
                 ctx.stroke();
@@ -83,9 +91,9 @@
               ctx.beginPath();
               ctx.fillStyle = "red";
               ctx.moveTo(0, 0);
-              ctx.lineTo(-6, -30);
-              ctx.lineTo(0, -100);
-              ctx.lineTo(6, -30);
+              ctx.lineTo(-12, -60);
+              ctx.lineTo(0, -200);
+              ctx.lineTo(12, -60);
               ctx.closePath();
               ctx.fill();
               ctx.restore();
@@ -97,9 +105,9 @@
               ctx.beginPath();
               ctx.fillStyle = "yellow";
               ctx.moveTo(0, 0);
-              ctx.lineTo(-4, -40);
-              ctx.lineTo(0, -120);
-              ctx.lineTo(4, -40);
+              ctx.lineTo(-8, -80);
+              ctx.lineTo(0, -240);
+              ctx.lineTo(8, -80);
               ctx.closePath();
               ctx.fill();
               ctx.restore();
@@ -109,9 +117,9 @@
               ctx.beginPath();
               ctx.fillStyle = "black";
               ctx.moveTo(0, 0);
-              ctx.lineTo(-5, -45);
-              ctx.lineTo(0, -135);
-              ctx.lineTo(5, -45);
+              ctx.lineTo(-10, -90);
+              ctx.lineTo(0, -270);
+              ctx.lineTo(10, -90);
               ctx.closePath();
               ctx.fill();
               ctx.restore();
@@ -119,22 +127,87 @@
               //画中心
               ctx.beginPath();
               ctx.fillStyle = "blue";
-              ctx.arc(0,0,10,0,2*Math.PI);
+              ctx.arc(0,0,20,0,2*Math.PI);
               ctx.fill();
               ctx.restore();
             },1000);
-          }
+          },
+          circle1:function (el) {
+                setInterval(function () {
+                    const ctx=el.getContext("2d");
+                    ctx.clearRect(0, 0, 600, 600);
+                    //console.log(ctx);
+                    //修改坐标原点
+                    ctx.save();
+                    ctx.translate(310, 310);
+                    const date = new Date();
+                    var hours = date.getHours();
+                    var minutes = date.getMinutes();
+                    var second = date.getSeconds();
+                    if(date.getHours()>12){
+                        hours-=12;
+                    }
+                    //画时针
+                    var hoursAngle = (Math.PI / 180)*30*hours + (Math.PI / 6)*(minutes/60);
+                    ctx.save();
+                    ctx.rotate(hoursAngle);
+                    ctx.beginPath();
+                    ctx.fillStyle = "red";
+                    ctx.moveTo(0, 0);
+                    ctx.lineTo(-12, -60);
+                    ctx.lineTo(0, -200);
+                    ctx.lineTo(12, -60);
+                    ctx.closePath();
+                    ctx.fill();
+                    ctx.restore();
+
+                    //画分针
+                    var minutesAngle = (Math.PI / 180)*minutes*6 + (Math.PI / 30)*(second/60);
+                    ctx.save();
+                    ctx.rotate(minutesAngle);
+                    ctx.beginPath();
+                    ctx.fillStyle = "yellow";
+                    ctx.moveTo(0, 0);
+                    ctx.lineTo(-8, -80);
+                    ctx.lineTo(0, -240);
+                    ctx.lineTo(8, -80);
+                    ctx.closePath();
+                    ctx.fill();
+                    ctx.restore();
+                    //画秒针
+                    ctx.save();
+                    ctx.rotate((Math.PI / 180)*second*6);
+                    ctx.beginPath();
+                    ctx.fillStyle = "black";
+                    ctx.moveTo(0, 0);
+                    ctx.lineTo(-10, -90);
+                    ctx.lineTo(0, -270);
+                    ctx.lineTo(10, -90);
+                    ctx.closePath();
+                    ctx.fill();
+                    ctx.restore();
+
+                    //画中心
+                    ctx.beginPath();
+                    ctx.fillStyle = "blue";
+                    ctx.arc(0,0,20,0,2*Math.PI);
+                    ctx.fill();
+                    ctx.restore();
+                },1000);
+            }
         }
     }
 </script>
 
 <style scoped>
   .home{ background-color: #f7f7f7; padding-top: 20px}
-  .clock{width: 310px; height: 310px}
+  .clock{width: 310px; height: 310px; display: flex; align-items: center;justify-content: center}
+  .clock1{width: 310px; height: 310px; display: flex; align-items: center;justify-content: center; background: url(../assets/timg.jpg) no-repeat; background-size:100% 100%;  }
   .fade-enter-active, .fade-leave-active {
     transition: all .5s ease;
     transform: translateX(0vw);
   }
+  .line{width: 100%; height: 50px; margin: 0 auto; display: flex; align-items: center;justify-content: center;}
   .circle{width: 200px; height: 200px; margin: 0 auto;position: relative;}
   .circleTitle{position: absolute; top:50%; left: 50%; width: 40px; height: 40px; text-align: center; line-height: 40px;
     margin-left: -20px; margin-top: -20px; font-size: 20px}
